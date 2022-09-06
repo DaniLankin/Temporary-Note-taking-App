167d59c42aad0056eaae2c4e6839ce0d6e81014c
LankinD <daniellan@my.hit.ac.il>
2022-09-06 14:12:46 +0300

    Deploy

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_title = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.View = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.tb_message = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(102, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(102, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // tb_title
+            // 
+            this.tb_title.Location = new System.Drawing.Point(106, 144);
+            this.tb_title.Name = "tb_title";
+            this.tb_title.Size = new System.Drawing.Size(100, 20);
+            this.tb_title.TabIndex = 2;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(72, 247);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 5;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(188, 247);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 6;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // View
+            // 
+            this.View.Location = new System.Drawing.Point(497, 281);
+            this.View.Name = "View";
+            this.View.Size = new System.Drawing.Size(75, 23);
+            this.View.TabIndex = 7;
+            this.View.Text = "View";
+            this.View.UseVisualStyleBackColor = true;
+            this.View.Click += new System.EventHandler(this.View_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(597, 281);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 8;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(497, 69);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(240, 191);
+            this.dgv.TabIndex = 10;
+            // 
+            // tb_message
+            // 
+            this.tb_message.Location = new System.Drawing.Point(106, 190);
+            this.tb_message.Multiline = true;
+            this.tb_message.Name = "tb_message";
+            this.tb_message.Size = new System.Drawing.Size(239, 51);
+            this.tb_message.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_message);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.View);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.tb_title);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_title;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button View;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox tb_message;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,57 @@ namespace Temporary_Note_taking_App
 {
     public partial class Form1 : Form
     {
+        DataTable table;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Message", typeof(String));
+
+            dgv.DataSource = table;
+
+            dgv.Columns["Message"].Visible = false;
+            dgv.Columns["Title"].Width = 183;
+            
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (!(tb_title.Text == "" || tb_message.Text == ""))//if not empty
+            {
+                table.Rows.Add(tb_title.Text, tb_message.Text);
+                tb_title.Clear();
+                tb_message.Clear();
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            tb_title.Clear();
+            tb_message.Clear();
+        }
+
+        private void View_Click(object sender, EventArgs e)
+        {
+            int index = dgv.CurrentCell.RowIndex;
+
+            if (index > -1)
+            {
+                tb_title.Text = table.Rows[index][0].ToString();
+                tb_message.Text = table.Rows[index][1].ToString();
+            }
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            int index = dgv.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
         }
     }
 }
